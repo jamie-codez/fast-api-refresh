a74ae3a98c6f74de9baa065d47baa3e04e7897a1
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-05 22:10:06 +0300

    Query parameter

--- app.py
+++ app.py
@@ -17,5 +17,14 @@ def index():
 
 # Path parameter
 @app.get("/students/{student_id}")
-def get_student_by_id(student_id: int = Path(None, description="The id of the student to be retrieved",gt=0)):
+def get_student_by_id(student_id: int = Path(None, description="The id of the student to be retrieved", gt=0)):
     return students[student_id]
+
+
+# Query parameters
+@app.get("/students")
+def get_students_by_name(name: str):
+    for student_id in students:
+        if students[student_id][name] == name:
+            return students[student_id]
+    return {"message": "No data found"}

6aa80f13740519b54524004b294ed62dc2eedc9c
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-05 21:35:23 +0300

    Path paramenter

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "final year"
+    }
+}
 
 
 @app.get("/")
 def index():
     return {"message": "Server is app and running"}
+
+
+# Path parameter
+@app.get("/students/{student_id}")
+def get_student_by_id(student_id: int):
+    return students[student_id]

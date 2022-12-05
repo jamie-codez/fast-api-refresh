2cd47ff68ce3388cd52a91c1dfd22d6982b7e2ad
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-05 23:51:54 +0300

    Delete route with id

--- app.py
+++ app.py
@@ -80,3 +80,12 @@ def update_student(student_id: int, student: UpdateStudent):
         students[student_id].year = student.year
     # students[student_id] = student
     return students[student_id]
+
+
+# Delete methods
+@app.delete("/student/delete/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"message": "Student does not exist"}
+    del students[student_id]
+    return {"message": "Student deleted successfully"}
